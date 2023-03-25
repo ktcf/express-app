f032a5886799ce09f7c016f151d286aa95977b8e
ktcf <5641897+ktcf@users.noreply.github.com>
2023-03-25 21:01:41 +0200

    Update message to be sure

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hola Mundo!</h1>')
+  res.send('<h1>Is this thing on?</h1>')
 })
 
 const PORT = 8080
